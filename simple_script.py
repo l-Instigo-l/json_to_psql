2aa140344059c840a047eaa68d1a08e752845386
l-Instigo-l <39703459+l-Instigo-l@users.noreply.github.com>
2019-04-26 20:31:19 +0500

    Update simple_script.py

--- simple_script.py
+++ simple_script.py
@@ -8,9 +8,11 @@ engine = create_engine('postgresql://username:password@localhost/postgres')
 db = scoped_session(sessionmaker(bind=engine))
 
 def json_to_psql():
+	# Получение json через api
 	#json_data = requests.get("http://url-to-api")
 	#data = json_data.json()
 
+	# Чтение json из файла
 	with open('json_data.json', 'r') as f:
 	        data = json.load(f)
 
@@ -19,11 +21,14 @@ def json_to_psql():
 	Departure_date = data['Departure_Date']
 	Customers_data = json.dumps(data['Customers_data'])
 
+	print(Origin, Destination, Departure_date, Customers_data)
+
 	db.execute("INSERT INTO test_table (origin, destination, departure_date, customers_data) VALUES (:origin, :destination, :departure_date, :customers_data)", {"origin": Origin, "destination": Destination, "departure_date": Departure_date, "customers_data": Customers_data})
 	db.commit()
 
 def psql_to_json(id):
 	# Выбор строки таблицы осуществляется по id
+	
 	data = db.execute("SELECT * FROM test_table WHERE id = :id ", {"id": id}).fetchone()
 
 	Origin = data.origin
@@ -32,11 +37,11 @@ def psql_to_json(id):
 	Customers_data = data.customers_data
 
 	d = { "Origin": Origin, "Destination": Destination, "Departure_Date" : Departure_date, "Customers_data": Customers_data}
+	print(d)
 	json_data = json.dumps(d)
 
-
-
-
+	# Отправляем post-request и сохраняем ответ сервера
+	r = requests.post(url = API_ENDPOINT, data = json_data) 
 
 
 
